e8fdf51cd80fb6bc24fc5424258411b98de5b876
He Lin <helinchooi@gmail.com>
2022-03-21 13:37:02 +0800

    Make blog page the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 
-const Blog = () => {
+export const query = graphql`
+  query {
+    wpgraphql {
+      posts {
+        nodes {
+          id
+          title
+          uri
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+const Blog = ({ data }) => {
+  const posts = data.wpgraphql.posts.nodes
 
   return (
     <Layout>
-      <h2>Aglowlife</h2>
-      <Link
-        to={`/blog`}
-      >View blog page</Link>
+      {posts.map(post => (
+        <article key={post.id}>
+          <h2>
+            <Link
+              to={`/blog/${post.uri}`}
+              dangerouslySetInnerHTML={{ __html: post.title }}
+            />
+          </h2>
+          <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+        </article>
+      ))}
     </Layout>
   )
 }
