25d9b133572a6d6a7d3580ebee11f10278a0df75
He Lin <helinchooi@gmail.com>
2022-03-22 16:58:30 +0800

    Fix GA not working by adding html.js

---
+++ src/html.js
@@ -0,0 +1,25 @@
+{/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-WMENNNDWMY"></script> */ }
+{/* <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WMENNNDWMY');
+</script> */}
+<>
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-WMENNNDWMY"
+  />
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    
+    gtag('config', 'G-WMENNNDWMY');
+    `,
+    }}
+  />
+</>
